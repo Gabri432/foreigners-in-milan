b4ceae7bb08c915a7baa9bb8d2d531a9086603dd
Gabriele Gatti <gabrielegatti432@gmail.com>
2025-04-23 20:36:58 +0200

    Adding new datasets and graphs.

---
+++ scripts/evolution_of_crimes.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+total_crimes = pd.read_csv("datasets/Reati_totali_serie_storica.csv", delimiter=',')
+people = pd.read_csv('datasets/ds75_stranieri_sesso_citt.csv', sep=';', index_col=False)
+
+year = 2023
+start_year = 2004
+
+def count_people_per_year(year):
+    people_per_year = people[people['Anno'].eq(year)]
+    total_per_country = people_per_year.groupby('Cittadinanza')['Residenti'].sum()
+    sum = 0
+    for country in total_per_country:
+        sum += country
+    return sum
+
+def sequence(starting_year, ending_year, jump_years):
+    begin = starting_year
+    amounts = {'year': [], 'total':[]}
+    while begin < ending_year+jump_years:
+        amounts["total"].append(count_people_per_year(begin)/1000)
+        amounts["year"].append(begin)
+        begin = begin+jump_years
+
+    return amounts
+
+def get_crimes_per_year(starting_year, last_year):
+    amounts = {'year': [], 'total':[]}
+    foreigners_amount = sequence(starting_year, last_year, 1)
+    while starting_year <= last_year:
+        crimes_this_year = total_crimes[total_crimes["anno_rilevamento_reato"].eq(starting_year) &
+                                              total_crimes["Reati_denunciati_tipologia"].eq("Reati totale")] 
+        crimes_sum = crimes_this_year.groupby('Reati_denunciati_tipologia')['reati_denunciati'].sum()
+        total = 0
+        for crimes in crimes_sum:
+            total+=int(crimes)
+        
+        amounts["total"].append(total/1000)
+        amounts["year"].append(starting_year)
+        starting_year = starting_year+1
+
+    fig, ax1 = plt.subplots(figsize=(12, 6))
+    ax2 = ax1.twinx()
+
+    ax1.set_xlabel("Anno")
+    ax1.plot(amounts['year'], amounts['total'], color='skyblue', zorder=2, alpha=0.7, linestyle = 'dashed', label="Crimini")
+    ax2.plot(foreigners_amount['year'], foreigners_amount['total'], color='blue', zorder=2, alpha=0.7, linestyle = 'dashed', label="Popolazione straniera")
+    ax1.set_ylabel("Crimini (in migliaia)")
+    ax2.set_ylabel("Popolazione straniera (in migliaia)")
+
+    fig.suptitle("Andamento popolazione stranieria e criminalità", fontsize=20)
+    fig.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax1.transAxes)
+    #plt.figure(figsize=(12, 6))
+    #plt.plot(amounts['year'], amounts['total'], color='skyblue', zorder=2, alpha=0.7, linestyle = 'dashed')
+    #plt.xlabel('Anno')
+    plt.xticks(amounts['year'])
+    #plt.ylabel('Crimini (in migliaia)')
+    #plt.tight_layout()
+    plt.show()
+
+get_crimes_per_year(start_year, year)
---
+++ scripts/foreign_detainees.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+total_detainees = pd.read_csv("datasets/detenuti_stranieri_2010_2020.csv", delimiter=',')
+
+year = 2020
+start_year = 2010
+
+def get_detainees_per_year(starting_year, last_year):
+    amounts = {'year': [], 'total':[]}
+    while starting_year <= last_year:
+        detainees_this_year = total_detainees[total_detainees["anno_rilevazione_detenuti"].eq(starting_year) &
+                                              total_detainees["indicatori"].eq("detenuti stranieri totale")] 
+        detainees_sum = detainees_this_year.groupby('indicatori')['detenuti'].sum()
+        total = 0
+        for detainees in detainees_sum:
+            total+=detainees
+        
+        amounts["total"].append(total)
+        amounts["year"].append(starting_year)
+        starting_year = starting_year+1
+    
+    plt.figure(figsize=(12, 6))
+    #plt.bar(amounts['year'], amounts['total'], color='skyblue', zorder=2, alpha=0.5)
+    plt.plot(amounts['year'], amounts['total'], color='skyblue', zorder=2, alpha=0.7, linestyle = 'dashed')
+    plt.xlabel('Anno')
+    plt.ylabel('Detenuti Stranieri')
+    plt.tight_layout()
+    plt.show()
+
+get_detainees_per_year(start_year, year)
